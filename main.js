ba2e66189368f233d64f1e2199dc0dcfee35ff8c
Stuart Hamblin <stuart@Stuarts-MacBook-Air.local>
2018-12-08 21:45:13 -0600

    materialize start

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-"use strict";
+var instanceSidenav = M.Sidenav.getInstance(elem);
+var instanceCarousel = M.Carousel.getInstance(elem);
+$(document).ready(function(){
+    $('.carousel').carousel({duration:300});
+});
 
